e871fc086d8d1a73eabf7e1b25f63b149af98701
xct <xcat182@gmail.com>
2021-03-14 19:38:58 +0300

    feat: add function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let brackets = str.split('');
+    let config = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        config.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < brackets.length; i++) {
+        for (let j = 0; j < config.length; j++) {
+            if ((brackets[i - 1] + brackets[i]) == config[j]) {
+                brackets.splice(i - 1, 2);
+                i = i - 2;
+                break;
+            }
+        }
+    }
+
+    return (brackets.length == 0);
 }
